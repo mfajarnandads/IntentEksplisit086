a827aa98097f135c735249a4014d1501d408656e
USER <nandaxd1140@gmail.com>
2022-06-21 21:29:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intenteksplisit086/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.intenteksplisit086
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val etNim = findViewById<EditText>(R.id.etNim)
+        val etNama = findViewById<EditText>(R.id.etNama)
+        val btnProses = findViewById<Button>(R.id.btnProses)
+
+        btnProses.setOnClickListener{
+            if ("${etNim.text}".isNotEmpty() && "${etNama.text}".isNotEmpty()) {
+                val i = Intent(this@MainActivity, TampilDataActivity::class.java)
+                i.putExtra("NIM", "${etNim.text}")
+                i.putExtra("Nama", "${etNama.text}")
+                startActivity(i)
+            } else
+                Toast.makeText(this@MainActivity, "NIM atau nama belum diisi", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
